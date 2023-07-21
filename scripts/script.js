efad2a4bb9591ace5b5fdeab5f14d7f54b6d3a8d
a.blonskyi <ablonskyi.developer@gmail.com>
2023-07-22 00:26:35 +0300

    hw9 fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,33 @@
 let arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 
 let sumPositiveElements = 0;
+let positiveElementsCount  = 0;
 for (let i = 0; i <= arr.length; i++) {
     if (arr[i] > 0) {
         sumPositiveElements += arr[i];
+        positiveElementsCount++;
     }
 }
-alert(sumPositiveElements);
+alert(`${sumPositiveElements}\nКількість: ${positiveElementsCount}`);
+
+let minElement = arr[0];
+let iNumber = 0;
+for (let i = 1; i <= arr.length; i++) {
+    if (arr[i] < minElement) {
+        minElement = arr[i];
+        iNumber = i;
+    }
+}
+alert(`${minElement}\nПорядковий номер ${iNumber}`);
 
 let maxElement = arr[0];
 for (let i = 1; i <= arr.length; i++) {
     if (arr[i] > maxElement) {
         maxElement = arr[i];
+        iNumber = i;
     }
 }
-alert(maxElement);
+alert(`${maxElement}\nПорядковий номер ${iNumber}`);
 
 let negotiveElementsCount = 0;
 for(let i = 0; i <= arr.length; i++) {
@@ -43,7 +56,7 @@ alert(pairedPositiveElementsCount);
 let sumPairedPositiveElements = 0;
 for(let i = 0; i <= arr.length; i++) {
     if (arr[i] > 0 && arr[i] % 2 == 0) {
-        sumPairedPositiveElements += sumPairedPositiveElements;
+        sumPairedPositiveElements += arr[i];
     }
 }
 alert(sumPairedPositiveElements);
@@ -51,7 +64,7 @@ alert(sumPairedPositiveElements);
 let sumUnpairedPositiveElements = 0;
 for(let i = 0; i <= arr.length; i++) {
     if (arr[i] > 0 && arr[i] % 2 != 0) {
-        sumUnpairedPositiveElements += sumUnpairedPositiveElements;
+        sumUnpairedPositiveElements += arr[i];
     }
 }
 alert(sumUnpairedPositiveElements);
